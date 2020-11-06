5c8ba28b670272baabcaf5e13c53cc6afcd736aa
mcidoni <matthewcidoni@gmail.com>
2020-11-06 15:55:23 -0500

    updated curl scripts for songs

--- curl-scripts/auth/song_curl_scripts/show-song.sh
+++ curl-scripts/auth/song_curl_scripts/show-song.sh
@@ -1,7 +1,7 @@
 API="http://localhost:4741"
 URL_PATH="/songs"
 
-curl "${API}${URL_PATH}/${ID}" \
+curl "${API}${URL_PATH}" \
   --include \
   --request GET \
   --header "Authorization: Bearer ${TOKEN}"
